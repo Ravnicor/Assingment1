2c59e48bdb48bc6fc01d1c88c28aa39b5eba19f7
Ravnicor <35827154+Ravnicor@users.noreply.github.com>
2018-01-26 17:59:56 -0600

    Add files via upload

---
+++ myName.c
@@ -0,0 +1,18 @@
+/*Christopher Carreras
+*1/26/2018
+*Assignment 1, Program 5b
+*This program will output my name, with the intention
+*of being modified by a partner to output their name as well.
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main (int argc, char **argv) {
+
+        printf ("My name is Christopher Carreras.\n");
+		printf ("You used my program.\n");
+		printf ("Prepare to die.\n");
+		
+	return 0;
+}
