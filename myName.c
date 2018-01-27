77cdb12857977aa4ca0d5474e77f679a9cc0457e
regger3 <35748120+regger3@users.noreply.github.com>
2018-01-26 22:41:26 -0600

    Update myName.c

--- myName.c
+++ myName.c
@@ -1,4 +1,5 @@
 /*Christopher Carreras
+*Robert Egger
 *1/26/2018
 *Assignment 1, Program 5b
 *This program will output my name, with the intention
@@ -13,6 +14,9 @@ int main (int argc, char **argv) {
         printf ("My name is Christopher Carreras.\n");
 		printf ("You used my program.\n");
 		printf ("Prepare to die.\n");
-		
+	printf ("Or is my name is Robert Egger.\n");
+		printf ("I used Christopher's program.\n");
+		printf ("My github repository is not-trash.\n");
+	
 	return 0;
 }
